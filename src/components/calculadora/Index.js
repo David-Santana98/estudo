c7b8819fc083d9aca434f21357d930016a113332
David Santana <91860681+David-Santana98@users.noreply.github.com>
2023-03-11 13:05:18 -0300

    crei as arrows e a function calcular

--- src/components/calculadora/Index.js
+++ src/components/calculadora/Index.js
@@ -1,6 +1,23 @@
+import { useState } from "react";
 import "./Index.css";
 
 export default function Calculadora() {
+    const [qtd, setQtd] = useState(0);
+    const [preco, setPreco] = useState(0);
+    const [meia, setMeia] = useState(false);
+    const [total, setTotal] = useState(0);
+
+    function Calcular {
+        let tot = 0;
+        if (meia === true) {
+
+        }else {
+            
+        }    
+    }
+
+
+
   return (
     <div className="calculadora">
       <h1> Calculadora de Ingressos </h1>
@@ -8,14 +25,14 @@ export default function Calculadora() {
       <div>
         <label>Quantidade:</label>
 
-        <input type="text" />
+        <input type="text" onChange={e => setQtd(Number(e.target.value))}/>
       </div>
       <div>
         <label>Preço:</label>
-        <input type="text" />
+        <input type="text" onChange={e =>setPreco(Number(e.target.value))} />
       </div>
       <div>
-        <label>Meia</label> <input type="checkbox" />
+        <label>Meia</label> <input type="checkbox" onChange={e => setMeia(Number(e.target.checked))}/>
       </div>
 
       <div>
