221b7a19480009502dea989101ade51ca7f562e7
David Santana <91860681+David-Santana98@users.noreply.github.com>
2023-03-20 17:12:28 -0300

    Renderização condicional

--- src/Renderizacao.js
+++ src/Renderizacao.js
@@ -2,14 +2,31 @@ import { useState } from "react";
 
 export default function Renderizacao() {
   const [num, setNum] = useState(0);
+  const [exibir, setExibir] = useState(false);
 
   function Incr() {
     setNum(num + 1);
   }
 
+  function Abrir() {
+    setExibir(true);
+
+  }
+
   return (
     <div>
-      <h1>Renderização</h1>
+      <h1>Biscoito da Sorte</h1>
+      
+      <button onClick={Abrir}>Abrir biscoito da sorte </button>
+
+        {exibir === true &&
+        <div>
+            <h2>Você ganhou um carro!!!</h2> 
+            <h2>Uma Ferrari!!</h2>
+        </div>
+        }
+
+      <hr/>
 
       <h2>{num}</h2>
 
