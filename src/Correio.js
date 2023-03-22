5398279d2ef3cb607eec782f0e3e77975342c82d
David Santana <91860681+David-Santana98@users.noreply.github.com>
2023-03-22 17:01:37 -0300

    usando a api do correio

--- src/Correio.js
+++ src/Correio.js
@@ -1,20 +1,31 @@
+import axios from "axios";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
-export default function Correio() {
+export default function Correio() { 
+  const [cep, setCep] =  useState('');
+  const [resultado, setResultado] =  useState('');
+
+  async function buscar() {
+    const r = await axios.get('https://viacep.com.br/ws/' + cep + '/json/')
+
+    setResultado(r.data.logradouro + ', ' + r.data.bairro + ' - ' + r.data.localidade);
+  }
+
   return (
     <div>
       <h1>Correio</h1>
 
       <div>Informe seu CEP:</div>
       <div>
-        <input type="text" />
-        <button> Buscar</button>
+        <input type="text" value={cep} onChange={e => setCep(e.target.value)} />
+        <button onClick={buscar}> Buscar</button>
       </div>
       <div>
-        <h3></h3>
+          <h3> {resultado} </h3>
       </div>
 
-      <Link to='/'> Voltar para home </Link>
+      <Link to="/"> Voltar para home </Link>
     </div>
   );
 }
